8660fdb91acf6505768fb7c53c60208325a8a041
AdithaBuwaneka <adithabuwaneka0@gmail.com>
2025-09-19 03:19:10 +0530

    feat: initialize Agent_App with essential configurations and scripts
    - Added package.json with dependencies for Expo, React, and navigation.
    - Created postcss.config.js for Tailwind CSS integration.
    - Implemented reset-project.js script to reset project structure and manage directories.
    - Configured Tailwind CSS in tailwind.config.js.
    - Set up TypeScript configuration in tsconfig.json.

---
+++ Agent_App/app/index.tsx
@@ -0,0 +1,10 @@
+
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="bg-slate-100 rounded-xl p-4">
+      <Text className="text-lg font-medium bg-orange-800 text-white p-2 rounded">Welcome to Tailwind</Text>
+    </View>
+  );
+}
